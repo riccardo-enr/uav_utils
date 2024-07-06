026c64c4a6b77403026cc48cf698aa42fa5f2798
Riccardo Enrico <riccardo.enrico97@gmail.com>
2024-07-07 00:12:20 +0200

    update: uav_utils

--- python/control/nmpc_utils.py
+++ python/control/nmpc_utils.py
@@ -1,9 +1,15 @@
 import numpy as np
 
-def set_mpc_target_pos(NmpcNode):
+def set_mpc_target_pos(NmpcNode, position_pts : None, attitude_pts : None):
     # Mock trajectory for testing
-    position_pts = np.array([[0.0, 0.0, 1.0]] * (NmpcNode.N + 1))  # Example position setpoint
-    attitude_pts = np.array([[1.0, 0.0, 0.0, 0.0]] * (NmpcNode.N + 1))  # Example attitude setpoint
+    if position_pts is None:
+        position_pts = np.array([[0.0, 0.0, 1.0]] * (NmpcNode.N + 1))  # Example position setpoint
+    else:
+        position_pts = position_pts
+    if attitude_pts is None:
+        attitude_pts = np.array([[1.0, 0.0, 0.0, 0.0]] * (NmpcNode.N + 1))  # Example attitude setpoint
+    else:
+        attitude_pts = attitude_pts
 
     # Set parameters for the solver
     for j in range(NmpcNode.solver.N + 1):
@@ -14,11 +20,14 @@ def set_current_state(NmpcNode):
     
     """aggregates individual states to combined state of system
     """
-    
-    NmpcNode.current_state = np.concatenate((NmpcNode.position, NmpcNode.attitude, NmpcNode.velocity, NmpcNode.angular_velocity, NmpcNode.thrust, NmpcNode.state_timestamp), axis=None)
+    # , NmpcNode.state_timestamp
+    NmpcNode.current_state = np.concatenate((NmpcNode.position, NmpcNode.attitude, NmpcNode.velocity, NmpcNode.angular_velocity, NmpcNode.thrust), axis=None)
 
     #self.imu_data = np.concatenate((self.linear_accel_real, self.angular_accel_real, self.imu_timestamp), axis=None)
     
+    if NmpcNode.state_history is None:
+        NmpcNode.state_history = []
+        
     NmpcNode.state_history.append(NmpcNode.current_state)
     #self.imu_history.append(self.imu_data) 
 
@@ -31,6 +40,15 @@ def thrust_to_motor_values(thrust):
     # max_thrust
     max_thrust = thrust_constant * max_rotor_speed**2
     u_motor_values = thrust / max_thrust
+    # u_motor_values = - u_motor_values
+    
+    max_input = 0.9
+    
+    for i in range(len(u_motor_values)):
+        if u_motor_values[i] > max_input:
+            u_motor_values[i] = max_input
+        elif u_motor_values[i] < 0:
+            u_motor_values[i] = 0
 
     return u_motor_values
     
